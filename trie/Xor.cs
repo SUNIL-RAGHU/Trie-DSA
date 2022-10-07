9ec595dbf31ed034fe3acd2f9284ad387c74e737
SUNIL <sunilraghu199@gmail.com>
2022-10-07 18:52:17 +0530

    DSA-TRIE

---
+++ trie/Xor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace trie
+{
+	public class Xor
+	{
+		class Trie
+        {
+			TrieNode root;
+
+            public Trie()
+            {
+                root = new TrieNode();
+            }
+
+            public void insert(int num)
+            {
+                TrieNode curNode = root;
+
+                for(int i = 31; i >= 0; i--)
+                {
+                    int bit = num >> i & 1;
+
+                    if (curNode.nodes[bit] == null)
+                    {
+                        curNode.nodes[bit] = new TrieNode();
+                    }
+
+                    curNode = curNode.nodes[bit];
+                }
+            }
+
+
+            public int getMax(int num)
+            {
+
+            }
+
+        }
+
+
+        class TrieNode
+        {
+            public TrieNode[] nodes;
+
+            public TrieNode()
+            {
+                nodes = new TrieNode[2];
+            }
+        }
+	}
+}
+
